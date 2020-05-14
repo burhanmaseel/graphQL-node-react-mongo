a367844f7384b3a34445b6475bd10c025cd7d28f
burhan_maseel <burhanmaseel@gmail.com>
2020-05-14 15:46:29 +0500

    GraphQL with mock data implemented

--- app.js
+++ app.js
@@ -5,15 +5,32 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 app.use('/graphql', graphqlHttp({
    schema: buildSchema(`
+      type Event {
+         _id: ID!
+         title: String!
+         description: String!
+         price: Float!
+         date: String!
+      }
+
+      input EventInput {
+         title: String!
+         description: String!
+         price: Float!
+         date: String!
+      }
+
       type RootQuery {
-         events: [String!]!
+         events: [Event!]!
       }
 
       type RootMutation {
-         createEvent(name: String): String
+         createEvent(eventInput: EventInput): Event
       }
 
       schema {
@@ -22,8 +39,23 @@ app.use('/graphql', graphqlHttp({
       }
    `),
    rootValue: {
-
-   }
+      events: () => {
+         return events;
+      },
+      createEvent: (args) => {
+         const event = {
+            _id: Math.random().toString(),
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: args.eventInput.date
+         };
+         console.log(event);
+         events.push(event);
+         return event;
+      }
+   },
+   graphiql: true
 }));
 
 app.get('/', (req, res, next) => {
