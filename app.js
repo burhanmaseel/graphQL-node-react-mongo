b8172dd48c9f5bc85335c8e8464192cfb7aa919c
burhan_maseel <burhanmaseel@gmail.com>
2020-05-15 23:09:05 +0500

    Set up Mongo DB and GraphQL

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
+const Event = require('./models/events');
 const app = express();
 
 const events = [];
@@ -40,19 +42,31 @@ app.use('/graphql', graphqlHttp({
    `),
    rootValue: {
       events: () => {
-         return events;
+         return Event.find()
+            .then(events => {
+               return events.map(event => {
+                  return { ...event._doc, _id: event.id }
+               });
+            })
+            .catch(err => {
+               console.log(err);
+               throw (err);
+            });
       },
       createEvent: (args) => {
-         const event = {
-            _id: Math.random().toString(),
+         const event = new Event({
             title: args.eventInput.title,
             description: args.eventInput.description,
             price: +args.eventInput.price,
-            date: args.eventInput.date
-         };
-         console.log(event);
-         events.push(event);
-         return event;
+            date: new Date(args.eventInput.date)
+         });
+         return event.save().then(result => {
+            console.log(result);
+            return { ...result._doc };
+         }).catch(err => {
+            console.log(err);
+            throw err;
+         });
       }
    },
    graphiql: true
@@ -60,7 +74,12 @@ app.use('/graphql', graphqlHttp({
 
 app.get('/', (req, res, next) => {
    res.send('Hello world');
-})
+});
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-2kvex.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+   console.log('Database Successfully Connected');
+}).catch(err => {
+   console.log(`Database Connection Error: ${err}`);
+});
 
 const port = process.env.PORT || 5000; //You can change the port by changing the static port number 5000 to any other port or you can define it in .env file
 app.listen(port, () => console.log(`Server is running on port ${port}`));
